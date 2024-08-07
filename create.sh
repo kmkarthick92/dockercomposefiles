3b6903670d79cbfeb82dcb85370741db4f7178ed
Karthick <karthick.babu.ericsson@gmail.com>
2024-08-07 09:25:34 +0530

    create file and folder strcuture

---
+++ create.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+mkdir ./apps
+cd ./apps
+mkdir ./immich-app
+mkdir ./portainer
+mkdir ./dozzle
+
+wget -O ./immich-app/docker-compose.yml https://raw.githubusercontent.com/kmkarthick92/dockercomposefiles/master/immich/docker-compose.yml
+wget -O ./immich-app/.env https://raw.githubusercontent.com/kmkarthick92/dockercomposefiles/master/immich/.env
+wget -O ./immich-app/hwaccel.transcoding.yml https://raw.githubusercontent.com/kmkarthick92/dockercomposefiles/master/immich/hwaccel.transcoding.yml
+wget -O ./portainer/docker-compose.yml https://raw.githubusercontent.com/kmkarthick92/dockercomposefiles/master/docker-compose-portainer.yml
+wget -O ./dozzle/docker-compose.yml https://raw.githubusercontent.com/kmkarthick92/dockercomposefiles/master/docker-compose-dozzle.yml
+
